90df6c3d3e13795524bd53efd5bd4351f8c57890
khanhlecodegym <khanh.le@codegym.vn>
2020-10-22 09:18:24 +0700

    Demo Edit Delete Category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -31,10 +31,74 @@ namespace PXUProduct.Controllers
         [HttpPost]
         public IActionResult Create(Category category)
         {
-            _appDbContext.Categories.Add(category);
+            if(ModelState.IsValid)
+            {
+                _appDbContext.Categories.Add(category);
+                _appDbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _appDbContext.Categories.Find(id);
+            if(category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Categories.Update(category);
+                _appDbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _appDbContext.Categories.Find(id);
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCategory(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _appDbContext.Categories.Find(id);
+            if (category == null) return NotFound();
+
+            _appDbContext.Categories.Remove(category);
             _appDbContext.SaveChanges();
 
             return RedirectToAction("Index");
+
         }
     }
 }
